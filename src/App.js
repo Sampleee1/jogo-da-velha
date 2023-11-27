45b0266e83c0028af32c8b6e43c849a26a101ed4
Sampleee1 <fred.gamer.br1@gmail.com>
2023-11-27 18:10:44 -0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Aprendendo react
+          Aprendendo React
         </a>
       </header>
     </div>
